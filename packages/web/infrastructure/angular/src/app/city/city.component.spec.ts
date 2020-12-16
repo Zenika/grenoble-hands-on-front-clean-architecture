a8668e3f42c5a50ee2e2098f80a9bfbe8daca479
Martin Choraine <martin.choraine@gmail.com>
2020-12-16 22:18:48 +0100

    test components on angular

--- packages/web/infrastructure/angular/src/app/city/city.component.spec.ts
+++ packages/web/infrastructure/angular/src/app/city/city.component.spec.ts
@@ -1,49 +1,78 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { CityComponent } from './city.component';
+import {CityComponent} from './city.component';
 import {CityPresenterFactory} from "@grenoble-hands-on/web-adapters/dist/presenters/CityPresenterFactory";
 import {RouterTestingModule} from "@angular/router/testing";
 import {CityPresenterVM} from "@grenoble-hands-on/web-adapters";
+import {GeoPosition} from "@grenoble-hands-on/domain";
+import {By} from "@angular/platform-browser";
 
 describe('CityComponent', () => {
-  let component: CityComponent;
-  let fixture: ComponentFixture<CityComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CityComponent ],
-      providers: [
-        {
-          provide: CityPresenterFactory,
-          useValue: {
-            createCityPresenter() {
-              return {
-                vm: new CityPresenterVM(),
-                onVmUpdate() {
+  it('display header with city name', () => {
+    // Given
+    const vm = new CityPresenterVM()
+    vm.city = {name: 'Grenoble', position: new GeoPosition(45, 5)}
 
-                },
-                fetchCityWithWeather() {
+    // When
+    const {fixture} = mountComponent(vm);
 
-                }
+    // Then
+    const header = fixture.debugElement.query(By.css("h2")).nativeElement.textContent;
+    expect(header).toBe('Grenoble');
+  });
+
+  it('display daily weather with temperature', () => {
+    // Given
+    const vm = new CityPresenterVM()
+    vm.weather = [
+      {weather: 'sunny', temperatureMin: 8, temperatureMax: 15, day: new Date()}
+    ]
+
+    // When
+    const {fixture} = mountComponent(vm);
+
+    // Then
+    const weather = fixture.debugElement.queryAll(By.css("#daily-weather tr:not(:first-child)"));
+    const weatherCol = weather[0].queryAll(By.css("td"));
+    expect(weather.length).toBe(1)
+    // expect(weatherCol[0].nativeElement.textContent).toBe('day')
+    expect(weatherCol[2].nativeElement.textContent).toBe("15")
+    expect(weatherCol[3].nativeElement.textContent).toBe("8")
+  });
+
+
+});
+
+
+function mountComponent(vm: CityPresenterVM = new CityPresenterVM()) {
+  TestBed.configureTestingModule({
+    declarations: [CityComponent],
+    providers: [
+      {
+        provide: CityPresenterFactory,
+        useValue: {
+          createCityPresenter() {
+            return {
+              vm,
+              onVmUpdate() {
+
+              },
+              fetchCityWithWeather() {
+
               }
             }
           }
         }
-      ],
-      imports: [
-        RouterTestingModule
-      ]
-    })
+      }
+    ],
+    imports: [
+      RouterTestingModule
+    ]
+  })
     .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CityComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  const fixture = TestBed.createComponent(CityComponent);
+  const component = fixture.componentInstance;
+  fixture.detectChanges();
+  return {component, fixture};
+}
