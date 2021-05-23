8fa1f6f1eae8bcd5f939cdc911964f6cc3366335
Martin Choraine <martin.choraine@gmail.com>
2021-05-23 16:45:20 +0200

    feat: add weather bookmark to navbar

--- packages/infrastructure/angular/src/app/shared/components/navbar/navbar.component.ts
+++ packages/infrastructure/angular/src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core'
+import { NavbarController, NavbarControllerFactory, NavbarPresenterVM } from '@grenoble-hands-on/web-adapters'
+import { Observable } from 'rxjs'
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+    providers: [
+        {
+            provide: NavbarController,
+            useFactory: (presenterFactory: NavbarControllerFactory) => presenterFactory.build(),
+            deps: [NavbarControllerFactory]
+        }
+    ]
 })
 export class NavbarComponent implements OnInit {
+    vm$: Observable<NavbarPresenterVM> = new Observable<NavbarPresenterVM>(subscriber =>
+        this.controller.subscribeVM(vm => subscriber.next(vm))
+    )
 
-  constructor() { }
+    constructor(private readonly controller: NavbarController) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.controller.fetchBookmarkCityWeather()
+    }
 
 }
--- packages/infrastructure/angular/src/app/shared/components/weather-widget/weather-widget.component.spec.ts
+++ packages/infrastructure/angular/src/app/shared/components/weather-widget/weather-widget.component.spec.ts
@@ -1,25 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { WeatherWidgetComponent } from './weather-widget.component';
+import { WeatherWidgetComponent } from './weather-widget.component'
+import { DailyWeatherBuilder } from '@grenoble-hands-on/domain'
 
 describe('WeatherWidgetComponent', () => {
-  let component: WeatherWidgetComponent;
-  let fixture: ComponentFixture<WeatherWidgetComponent>;
+    let component: WeatherWidgetComponent
+    let fixture: ComponentFixture<WeatherWidgetComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WeatherWidgetComponent ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [WeatherWidgetComponent]
+        })
+                     .compileComponents()
     })
-    .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WeatherWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WeatherWidgetComponent)
+        component = fixture.componentInstance
+        component.weather = DailyWeatherBuilder.sunny()
+        fixture.detectChanges()
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
