0e78777bfb922d9ca9b08d48f9a7b2f3f7df8f06
Martin Choraine <martin.choraine@gmail.com>
2021-05-23 10:36:11 +0200

    feat: describe use case

---
+++ packages/domain/test/usecases/RetrieveBookmarkCityWeatherUseCase.spec.ts
@@ -0,0 +1,8 @@
+describe('Retrieve bookmark city weather use case', () => {
+
+    test.todo('notify weather of the day for grenoble when bookmarked city is Grenoble')
+
+    test.todo('notify no bookmarked city when have not bookmarked city')
+
+    test.todo('notify cannot retrieve weather city when repository is down')
+})
